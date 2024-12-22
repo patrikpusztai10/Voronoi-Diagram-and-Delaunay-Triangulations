53a43c544860d7b8d8152cf1f56e02d175f8973c
patrikpusztai10 <150380680+patrikpusztai10@users.noreply.github.com>
2024-12-22 20:43:43 +0200

    Update exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d, Delaunay
 import numpy as np
-
+def edges(delaunay):
+    edges=set()
+    for triangle in delaunay.simplices:
+        for i in range(3):
+            edge=tuple(sorted([triangle[i], triangle[(i + 1) % 3]]))
+            edges.add(edge)
+    return len(edges)
 M1= np.array([
     [-6, 4],
     [-6, 1],
@@ -33,8 +39,11 @@ del_tri3=Delaunay(M2_2)
 vor3 = Voronoi(M2_2)
 fig, ax= plt.subplots(1, 3, figsize=(16, 8))
 voronoi_plot_2d(vor1, ax=ax[0], show_vertices=False, line_colors='blue', line_width=2, point_size=8)
+halfline_count1 = sum(1 for vertices in vor1.ridge_vertices if -1 in vertices)
 voronoi_plot_2d(vor2, ax=ax[1], show_vertices=False, line_colors='blue', line_width=2, point_size=8)
+halfline_count2 = sum(1 for vertices in vor2.ridge_vertices if -1 in vertices)
 voronoi_plot_2d(vor3, ax=ax[2], show_vertices=False, line_colors='blue', line_width=2, point_size=8)
+halfline_count3= sum(1 for vertices in vor3.ridge_vertices if -1 in vertices)
 
 #M1
 ax[0].triplot(M1[:, 0], M1[:, 1], del_tri1.simplices, color='green')
@@ -60,4 +69,7 @@ ax[2].legend()
 ax[2].set_aspect('equal', adjustable='box')
 ax[2].set_xlim(-20, 10)
 ax[2].set_ylim(4, -30)
+print("1.",halfline_count1,"halflines and",edges(del_tri1),"edges")
+print("2.",halfline_count2,"halflines and",edges(del_tri2),"edges")
+print("3.",halfline_count3,"halflines and",edges(del_tri3),"edges")
 plt.show()
